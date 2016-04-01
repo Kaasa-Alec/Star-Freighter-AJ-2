3362e8a0a06961a9c673c0fabe325dc608cba9e2
JeffJones <JeffJones@jeffreys-mbp.attlocal.net>
2016-04-01 18:38:04 -0500

    4-01-16 changed GameControl, MapControl, added scenes, scene locations, Actors

--- Star-Freighter-AJ-2/src/byui/cit260/starfreighteraj/model/Actor.java
+++ Star-Freighter-AJ-2/src/byui/cit260/starfreighteraj/model/Actor.java
@@ -19,10 +19,11 @@ public enum Actor implements Serializable{
     Bill ("Vendor clerk, Guy at the counter."),
     Riley ("Ship mechanic."),
     Phillip ("First Officer of the ship, provides hints."),
-    Place_Holder_One ("Random encounter on the station."),
-    PlaceHolderTwo ("Provides job opportunity on the station."),
-    Smuggler ("Game enemy."),
-    Pirate ("Game enemy.");
+    Dr_Hu ("Works at the MedBay."),
+    Engineer_Bob ("Works at Manfacturing Center."),
+    Spice_Smuggler ("Game enemy."),
+    Space_Corsair ("Game enemy."),
+    Terror_Mites("Game enemy.");
 
     private final String description;
     private final Point coordinates;
